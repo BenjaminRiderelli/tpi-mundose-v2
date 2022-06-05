6a50ab4da3ba7ddf26de7141ef3e5e97799a8a0c
BenjaminRiderelli <riderelli.ben@gmail.com>
2022-06-05 15:41:43 +0200

    jelou

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -39,7 +39,7 @@ const FormSection = () => {
   
 
     const saveFormData = async () => {
-      const response = await fetch('https://api-mendoza.herokuapp.com/api/usuario', {
+      const response = await fetch('https://mendoza-apirest.herokuapp.com/api/usuario', {
         method: 'POST',
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(inputs),
